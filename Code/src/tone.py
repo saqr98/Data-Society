dd9a0c5db5c295727b81b92a6fb31c57971851a2
Sofia Molotkova <molotkova548@gmail.com>
2024-03-09 16:57:23 +0100

    for dynamic networks set default granularity to monthly

--- Code/src/tone.py
+++ Code/src/tone.py
@@ -11,7 +11,7 @@ from preprocess import dynamic, static, create_edges, \
                         create_undirected_network, create_nodes
 
 
-def tone(events: pd.DataFrame, dynam=False, mode=0, freq='D') -> pd.DataFrame:
+def tone(events: pd.DataFrame, dynam=False, mode=0, freq='M') -> pd.DataFrame:
     # Calculate edge weights using no. of mentions and average tone of an event
     events['Weight'] = calculate_weight(events['NumMentions'], events['AvgTone'], mode=mode)
     
